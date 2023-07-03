7bcc60b81cae787aa346bb2870d31d372b712a13
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-03 18:15:15 -0400

    Reworked dependencies to use selenium to connect to websites

--- scraper.py
+++ scraper.py
@@ -1,5 +1,17 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+from bs4 import BeautifulSoup
 
-driver = uc.Chrome() # Headless option does not work for chrome version > 108
-driver.get('https://nowsecure.nl')
-driver.save_screenshot('nowsecure.png')
+chrome_options= Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-gpu')
+
+driver=webdriver.Chrome(options=chrome_options)
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+driver.get('https://indeed.com')
+driver.save_screenshot('screencap.png')
