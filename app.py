52be019803a0db608f271bc43eefd98b93f36330
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-03 18:20:58 -0400

    Created boilerplate app.py Flask server file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+print("sneed")
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    Flask.run(app, "0.0.0.0", 8080, True)
