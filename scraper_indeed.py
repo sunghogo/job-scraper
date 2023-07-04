41c3b8359a1ee0626464eed6cb82826e01c9ce10
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-04 00:21:08 -0400

    scraper_indeed.py: Extracts job id, position, company, location, salary, link from listings on a single page in JSON format

--- scraper_indeed.py
+++ scraper_indeed.py
@@ -1,25 +1,53 @@
 import selenium
 from typing import Dict
+from bs4 import BeautifulSoup
 
 def scrape_indeed(driver: selenium.webdriver.chrome.webdriver.WebDriver, position: str, location: str, options: Dict[str, str] = None):
-    url = f'https://www.indeed.com/jobs?q={position.replace(" ", "+")}&l={location.replace(" ", "+")}'
+    jobs = []
+    
+    base_url = 'https://www.indeed.com'
+    url = f"{base_url}/jobs?q={position.replace(' ', '+')}&l={location.replace(' ', '+')}"
     if options != None:
         for key, value in options.items():
-            if key == "experience_level":
-                url += f'&sc=0kf%3Aexplvl({value})%3B'
+            if key == 'experience_level':
+                url += f"&sc=0kf%3Aexplvl({value})%3B"
             elif key == "date_posted":
-                url += f'&fromage={value}'
+                url += f"&fromage={value}"
             elif key == "sort_date":
-                url += f'&sort=date'
+                url += f"&sort=date"
             elif key == "page":
-                url += f'&start={value}'
+                url += f"&start={value}"
 
     driver.get(url)
     driver.save_screenshot('get-website-screencap.png')
 
-    # # Fetch HTML
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
+    # Fetch HTML
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    job_els = soup.find_all('table', class_='jobCard_mainContent')
+    
+    for job in job_els:
+        job_id = job.find('a').get('data-jk', None)
+        link = job.find('a').get('href', None)
+        position = job.find('h2', class_='jobTitle').get_text()
+        company = job.find('span', class_='companyName').get_text()
+        location = job.find('div', class_='companyLocation').get_text()
+        salary = job.find('div', class_='heading6 tapItem-gutter metadataContainer')
+        if salary != None:
+            salary = salary.get_text()
+       
+        job_dict = {
+            'job_id': job_id,       
+            'position': position,
+            'company': company,
+            'location': location,
+            'salary': salary,
+            'link': f"{base_url}/viewjob?jk={job_id}"
+        }
+        jobs.append(job_dict)
+        
+    print(jobs)
 
     # # Open the file in write mode
     # with open('output.html', 'w', encoding='utf-8') as file:
