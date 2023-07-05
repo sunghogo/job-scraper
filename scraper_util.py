a4eb6a5a2cc6a54baa3d6673b2cdc46027766328
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-05 16:14:47 -0400

    scraper_util.py: webdriver_screenshot() now uses screenshot_path var, renamed webdriver back driver due to WebDriver class confusion

--- scraper_util.py
+++ scraper_util.py
@@ -2,6 +2,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import logging
 import random
@@ -15,22 +16,22 @@ error_path = f"{outputs_path}/error"
 data_path = f"{outputs_path}/data"
 
 # Make webdriver wait until class loads, otherwise create error files in specified error directory and raise exception
-def webdriver_wait_class(webdriver: WebDriver, timeout: int, class_name: str, error_string: str = ""):
-    timout += random.random() * 1
+def webdriver_wait_class(driver: WebDriver, timeout: int, class_name: str, error_string: str = ""):
+    timeout += random.random() * 1
     try:
-        WebDriverWait(webdriver, timeout=timeout).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-    except:
-        webdriver.save_screenshot(f"{error_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_error.png")
+        WebDriverWait(driver, timeout=timeout).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+    except TimeoutException as err:
+        driver.save_screenshot(f"{error_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_error.png")
         with open(f"{error_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_error.html", 'w', encoding='utf-8') as file:
-            file.write(BeautifulSoup(webdriver.page_source, 'html.parser').prettify())          
+            file.write(BeautifulSoup(driver.page_source, 'html.parser').prettify())          
         logging.error(f"Loading timed out {timeout}s" if error_string == "" else f"Loading timed out {timeout}s: " + error_string)
-        raise Exception()
+        raise err
 
 # Saves screenshots in specified screenshots directory
 # webdriver.set_window_size() crashes the page
-def webdriver_screenshot(webdriver: WebDriver, filename: str):
-    filepath = f"outputs/screenshots/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_" + filename
-    webdriver.save_screenshot(filepath)
+def webdriver_screenshot(driver: WebDriver, filename: str):
+    filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}"
+    driver.save_screenshot(filepath)
     
 # Write output json in specified data directory
 def webdriver_write_data(data: List[Dict[str, str]]):
