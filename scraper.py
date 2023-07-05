d71d419304d30931b3cfd5a4b4a5a45d6c3503da
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-05 11:17:26 -0400

    scraper.py: Added named arguments for scrape_indeed() call

--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ def init_webdriver():
 driver = init_webdriver()
 
 # Start scraping
-scrape_indeed(driver, "Software Engineer", "United States", {"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true"})
+scrape_indeed(driver=driver, position="Software Engineer", location="United States", options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true"})
 
 # Close webdriver
 driver.quit()
