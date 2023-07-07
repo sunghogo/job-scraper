a3b66c2a13b81591cf4baed284e0ee9083e64510
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-07 17:56:23 -0400

    scraper.py: Added in scraping for New York, NY as tests

--- scraper.py
+++ scraper.py
@@ -8,7 +8,8 @@ from scraper_init import init_webdriver
 driver = init_webdriver()
 
 # Start scraping
-scrape_indeed(driver=driver, search_position="Software Engineer", search_location="United States", search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true"})
+scrape_indeed(driver=driver, search_position="Software Engineer", search_location="New York, NY", search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
+scrape_indeed(driver=driver, search_position="Software Engineer", search_location="United States", search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
 
 # Close webdriver
 driver.quit()
