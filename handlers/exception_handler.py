f67aeb1c0d7411448cdf9d560b7b34cb8b7d2e02
sunghogo <leinad5592@gmail.com>
2023-07-10 12:52:28 -0400

    Created exception_handler.py module with handle_exceptions_decorator() function that logs the exception message

---
+++ handlers/exception_handler.py
@@ -0,0 +1,14 @@
+import logging  # Setup logging config
+
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def handle_exceptions_decorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"Exception occurred: {str(e)}", exc_info=True)
+            raise e
+    return wrapper
