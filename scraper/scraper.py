6692a017186e5ebeb31e9c425ae397b8f68106ae
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-10 17:19:45 -0400

    scaper.py: Encapsulated functionality into scape_search() function to be called in app.py, fixed local imports to match running from app.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
-from scraper_indeed import scrape_indeed
-from scraper_init import init_webdriver
+from scraper.scraper_indeed import scrape_indeed
+from scraper.scraper_init import init_webdriver
 
-# Initialize webdriver instance
-driver = init_webdriver()
+def scrape_search(search_position: str, search_location: str):
+    # Initialize webdriver instance
+    driver = init_webdriver()
 
-# Start scraping
-scrape_indeed(driver=driver, search_position="Software Engineer", search_location="New York, NY", search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
-scrape_indeed(driver=driver, search_position="Software Engineer", search_location="United States", search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
-
-# Close webdriver
-driver.quit()
+    # Start scraping
+    scrape_indeed(driver = driver, search_position = search_position, search_location = search_location, search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
+    
+    # Close webdriver
+    driver.quit()
