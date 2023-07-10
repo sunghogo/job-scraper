eed066a5d08f95670e11e5b97cdffa8093696988
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-10 17:24:11 -0400

    app.py: Now calls scrape_search() to initiate scraping instead of from scraper.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from scraper.scraper import scrape_search
+
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-print("sneed")
-
 @app.route("/")
 def index():
+    scrape_search(search_position = "Software Engineer", search_location = "New York")
+    scrape_search(search_position = "Software Engineer", search_location = "United States")
     return render_template('index.html')
 
 if __name__ == "__main__":
