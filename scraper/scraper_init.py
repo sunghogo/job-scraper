1242e59457035035bbabfafbeddf50cfcbb53dcd
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-11 15:01:35 -0400

    scraper_init.py: Removed unused imports

--- scraper/scraper_init.py
+++ scraper/scraper_init.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import chromedriver_binary
 
 # Initializes Selenium chromium browser and settings
