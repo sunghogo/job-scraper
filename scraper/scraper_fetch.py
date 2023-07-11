df75c3324625e427b0b3b2532383361fe643091b
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-11 15:03:36 -0400

    scraper_fetch.py: Moved webdriver_wait_class() function from scraper_util.py module to scraper_fetch.py() for better organization

--- scraper/scraper_fetch.py
+++ scraper/scraper_fetch.py
@@ -3,9 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from scraper.scraper_util import webdriver_wait_class
+import random
+from handlers.exceptions_handlers import timeout_exceptions_handler
 
-# Fetches url, and refetches specified number of times after specified timeout
+# Make webdriver wait until class loads, otherwise raise Timeout Exception
+@timeout_exceptions_handler
+def webdriver_wait_class(driver: WebDriver, timeout: int, class_name: str):
+    # Increase timeout by a random number
+    timeout += random.random()
+    WebDriverWait(driver, timeout=timeout).until(
+        EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+    
+
+# Fetches url, wait until class loads, and refetches specified number of times after specified timeout
 def webdriver_fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, refetch_times: int = 0):
     # Fetch url
     driver.get(url)
