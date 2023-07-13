cf13a8d6f363399af0731b329b57c4de3f9381a5
sunghogo <leinad5592@gmail.com>
2023-07-13 12:26:38 -0400

    app.py: Flask now runs on port 5000

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from scraper.scraper import scrape_search
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+
 @app.route("/")
 def index():
-    scrape_search(search_position = "Software Engineer", search_location = "New York")
-    scrape_search(search_position = "Software Engineer", search_location = "United States")
+    scrape_search(search_position="Software Engineer",
+                  search_location="New York")
+    scrape_search(search_position="Software Engineer",
+                  search_location="United States")
     return render_template('index.html')
 
+
 if __name__ == "__main__":
-    Flask.run(app, "0.0.0.0", 8080, True)
+    Flask.run(app, "0.0.0.0", 5000, True)
