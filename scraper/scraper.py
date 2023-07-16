759a9b82199895ed69e32cfd006dc65b37bc58b3
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-16 19:35:46 -0400

    scraper.py: scrape_search() now takes in experience_level argument

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,12 @@
 from scraper.scraper_indeed import scrape_indeed
 from scraper.scraper_init import init_webdriver
 
-def scrape_search(search_position: str, search_location: str):
+def scrape_search(search_position: str, search_location: str, experience_level: str = "ALL"):
     # Initialize webdriver instance
     driver = init_webdriver()
 
     # Start scraping
-    scrape_indeed(driver = driver, search_position = search_position, search_location = search_location, search_options={"experience_level": "ENTRY_LEVEL", "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
+    scrape_indeed(driver = driver, search_position = search_position, search_location = search_location, search_options={"experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
     
     # Close webdriver
     driver.quit()
