d616368748cdad106a54b5acc4deb0c5f9a50eee
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-16 19:59:58 -0400

    app.py: Fixed searches to have more results

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
-    scrape_search(search_position = "Frontend Internship", search_location = "United States")
-    scrape_search(search_position = "Full Stack Internship", search_location = "New York")
-    scrape_search(search_position = "Software Engineer Internship", search_location = "New York")
+    scrape_search(search_position = "Software Engineer Internship", search_location = "United States")
     scrape_search(search_position="Software Engineer",
-                  search_location="New York", experience_level = "ENTRY_LEVEL")
+                  search_location="New York State", experience_level = "ENTRY_LEVEL")
+    scrape_search(search_position = "Frontend Developer", search_location = "United States", experience_level = "ENTRY_LEVEL")
+    scrape_search(search_position = "Full Stack Developer", search_location = "United States", experience_level = "ENTRY_LEVEL")
     # scrape_search(search_position="Software Engineer",
     #               search_location="United States", experience_level = "ENTRY_LEVEL")
     return render_template('index.html')
