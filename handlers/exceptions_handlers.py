f01c7913ed9e7177d86b9f0da00187e2e294dc47
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 09:13:16 -0400

    exceptions_handlers.py: Separated webdriver_screenshot() from timeout_exceptions_handler into its own timeout_exceptions_screenshot_handler

--- handlers/exceptions_handlers.py
+++ handlers/exceptions_handlers.py
@@ -30,8 +30,19 @@ def timeout_exceptions_handler(func):
         try:
             return func(*args, **kwargs)
         except TimeoutException:
-            webdriver_screenshot(
-                driver=kwargs['driver'], filename='timeout_exception')
             raise TimeoutException(
                 f"Waiting for {kwargs['class_name']} at {kwargs['driver'].current_url} timed out after {kwargs['timeout']}s")
+    return wrapper
+
+
+# Decorator that handles screenshotting from webdriver upon timeout exception, and then raising the exception upwards
+# Based on arguments passed to scraper_util.py: webdriver_wait_class(driver: WebDriver, timeout: int, class_name: str)
+def timeout_exceptions_screenshot_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except TimeoutException as e:
+            webdriver_screenshot(
+    driver=kwargs['driver'], filename='timeout_exception')
+            raise e
     return wrapper
