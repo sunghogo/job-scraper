c5bb295515f9fe5ba051ccefd89fb9c04bd9db82
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 14:48:08 -0400

    util.py: Corrected comments, read_json_data() now returns data

--- util/util.py
+++ util/util.py
@@ -3,7 +3,8 @@ import json
 from datetime import datetime
 import os
 
-# Declare outputs directory paths
+# Declare and initialize outputs directory and subdirectory paths
+# THESE PATHS ARE IMPORTED AND USED IN OTHER UTIL MODULES
 outputs_path = "outputs"
 screenshots_path = f"{outputs_path}/screenshots"
 errors_path = f"{outputs_path}/errors"
@@ -11,7 +12,7 @@ data_path = f"{outputs_path}/data"
 logs_path = f"{outputs_path}/logs"
 
 
-# Write/appends output json in specified data directory, returns filepath name
+# Writes json data in outputs data directory, returns filepath name
 def write_json_data(data: List[Dict[str, str]], filename: str, filepath: str = "") -> str:
     if filepath == "":
         full_filename = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.json"
@@ -21,15 +22,15 @@ def write_json_data(data: List[Dict[str, str]], filename: str, filepath: str = "
     return filepath
 
 
-# Opens output json file in specified data directory, returns the json data
+# Opens json file in outputs data directory, returns the json data
 def read_json_data(filename: str, filepath: str = "") -> List[Dict[str, str]]:
     if filepath == "":
         filepath = f"{data_path}/{filename}"
     with open(filepath, 'r') as file:
-        json.load(file)
+        return json.load(file)
 
 
-# Delete output json file in specified data directory
+# Delete json file in outputs data directory
 def delete_json_data(filename: str, filepath: str = ""):
     if filepath == "":
         filepath = f"{data_path}/{filename}"
@@ -37,7 +38,7 @@ def delete_json_data(filename: str, filepath: str = ""):
         os.remove(filepath)
 
 
-# Writes/Appends to txt logs in specified directory
+# Writes/Appends to logs in outputs logs directory
 def append_log(data: str, log_type: str, filename: str):
     full_filename = f"{datetime.now().strftime('%Y-%m-%d')}_{filename}.log"
     filepath = ""
