ce029bb8e8bacdc6b1b694ad6687ecc522b252de
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 14:41:56 -0400

    webdriver_util.py: Now actually contains the webdriver functions

--- util/webdriver_util.py
+++ util/webdriver_util.py
@@ -1,24 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from util.util import screenshots_path
+from datetime import datetime
+import random
 
-# Initializes Selenium chromium browser and settings
-def init_webdriver():
-    # Setup selenium chrome options
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    # Setting logging level for browser to 'WARNING' and above
-    chrome_options.add_argument("--log-level=3")
-    chrome_options.set_capability('goog:loggingPrefs', { 'browser':'WARNING' })
-
-    # Initialize webdriver
-    driver = webdriver.Chrome(options = chrome_options)
-    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+# Make webdriver wait until class loads, otherwise raise Timeout Exception
+def wait_class(driver: WebDriver, timeout: int, class_name: str):
+    # Increase timeout by a random number
+    timeout += random.random()
+    WebDriverWait(driver, timeout=timeout).until(
+        EC.presence_of_element_located((By.CLASS_NAME, class_name)))
     
-    return driver
+
+# Fetches url, wait until class loads, and refetches specified number of times after specified timeout
+def fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, refetch_times: int = 0):
+    # Fetch url
+    driver.get(url)
+    
+    exception = TimeoutException()
+    # Wait for page to load, and if it does, shortcircuit function and return
+    # Otherwise, after specified refetch_times, grab the most recent exception and raise it
+    for i in range(refetch_times):
+        try:
+            wait_class(driver = driver, timeout=timeout, class_name = class_name)
+            return
+        except TimeoutException as e:
+            exception = e
+            continue
+    
+    # Raise most recent timeout exception
+    raise exception
+
+
+# Saves screenshots in specified screenshots directory
+# webdriver.set_window_size() crashes the page
+def screenshot(driver: WebDriver, filename: str):
+    filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.png"
+    driver.save_screenshot(filepath)
