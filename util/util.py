3cc9aa471bde2a1a0ebdbf16663b0540699e89f3
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 14:40:30 -0400

    Moved webdriver functions from util.py module to webdriver_util.py module

--- util/util.py
+++ util/util.py
@@ -1,10 +1,3 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import random
-from handlers.exceptions_handlers import timeout_exceptions_handler
 from typing import Dict, List
 import json
 from datetime import datetime
@@ -16,41 +9,6 @@ screenshots_path = f"{outputs_path}/screenshots"
 errors_path = f"{outputs_path}/errors"
 data_path = f"{outputs_path}/data"
 logs_path = f"{outputs_path}/logs"
-
-
-# Make webdriver wait until class loads, otherwise raise Timeout Exception
-def wait_class(driver: WebDriver, timeout: int, class_name: str):
-    # Increase timeout by a random number
-    timeout += random.random()
-    WebDriverWait(driver, timeout=timeout).until(
-        EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-    
-
-# Fetches url, wait until class loads, and refetches specified number of times after specified timeout
-def fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, refetch_times: int = 0):
-    # Fetch url
-    driver.get(url)
-    
-    exception = TimeoutException()
-    # Wait for page to load, and if it does, shortcircuit function and return
-    # Otherwise, after specified refetch_times, grab the most recent exception and raise it
-    for i in range(refetch_times):
-        try:
-            wait_class(driver = driver, timeout=timeout, class_name = class_name)
-            return
-        except TimeoutException as e:
-            exception = e
-            continue
-    
-    # Raise most recent timeout exception
-    raise exception
-
-
-# Saves screenshots in specified screenshots directory
-# webdriver.set_window_size() crashes the page
-def screenshot(driver: WebDriver, filename: str):
-    filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.png"
-    driver.save_screenshot(filepath)
 
 
 # Write/appends output json in specified data directory, returns filepath name
