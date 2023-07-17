e29a723ac19da19cc1cd3e60113e81014898258c
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 14:54:15 -0400

    webdriver_util.pt: Fixed and cleaned up comments and whitespaces

--- util/webdriver_util.py
+++ util/webdriver_util.py
@@ -7,35 +7,29 @@ from util.util import screenshots_path
 from datetime import datetime
 import random
 
-# Make webdriver wait until class loads, otherwise raise Timeout Exception
+# Make webdriver wait until class loads, otherwise raises timeout exception
 def wait_class(driver: WebDriver, timeout: int, class_name: str):
-    # Increase timeout by a random number
     timeout += random.random()
     WebDriverWait(driver, timeout=timeout).until(
         EC.presence_of_element_located((By.CLASS_NAME, class_name)))
     
 
-# Fetches url, wait until class loads, and refetches specified number of times after specified timeout
+# Makes webdriver fetch url, wait until class loads, and refetches specified number of times 
+# If timing out all the refetches, raises most recent timeout exception
 def fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, refetch_times: int = 0):
-    # Fetch url
     driver.get(url)
-    
-    exception = TimeoutException()
-    # Wait for page to load, and if it does, shortcircuit function and return
-    # Otherwise, after specified refetch_times, grab the most recent exception and raise it
+    timeout_exception = TimeoutException()
     for i in range(refetch_times):
         try:
             wait_class(driver = driver, timeout=timeout, class_name = class_name)
             return
         except TimeoutException as e:
-            exception = e
+            timeout_exception = e
             continue
-    
-    # Raise most recent timeout exception
-    raise exception
+    raise timeout_exception
 
 
-# Saves screenshots in specified screenshots directory
+# Make webdriver screenshot and save the image in outputs screenshots directory
 # webdriver.set_window_size() crashes the page
 def screenshot(driver: WebDriver, filename: str):
     filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.png"
