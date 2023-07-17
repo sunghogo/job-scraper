aaf075338f5b922a8d4a36eeb0823ead743b5028
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 16:37:46 -0400

    app.py: Reformed test searches

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/")
 def index():
     scrape(search_position = "Frontend Internship", search_location = "United States")
-    scrape(search_position = "Full Stack Internship", search_location = "United States")
     scrape(search_position = "Software Engineer Internship", search_location = "United States")
     scrape(search_position="Software Engineer",
                   search_location="New York State", experience_level = "ENTRY_LEVEL")
