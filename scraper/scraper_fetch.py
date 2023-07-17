7eeaf8b8b08419690af509e137f911220f9427c8
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 15:27:00 -0400

    scraper_fetch.py: fetch_indeed() now properly catches NoResultsException, timeout raised to 15s

--- scraper/scraper_fetch.py
+++ scraper/scraper_fetch.py
@@ -4,14 +4,17 @@ from util.webdriver_util import fetch_wait_class
 from handlers.exceptions_handlers import NoResultsException
 
 # Fetches indeed, and raises a no results exception if there are no results, or a timeout exxception if the jobs do not load correctly
+# TIMEOUT AT 10S TRIGGERS RATE LIMIT
 def fetch_indeed(driver: WebDriver, url: str, initial_fetch: bool = False):
     if initial_fetch:
         try:
             fetch_wait_class(
-            driver=driver, url=url, class_name='jobsearch-NoResult-messageContainer', timeout=10)
+            driver=driver, url=url, class_name='jobsearch-NoResult-messageContainer', timeout=15)
             raise NoResultsException()
+        except NoResultsException as e:
+            raise e
         except TimeoutException:
             pass
     
     fetch_wait_class(
-        driver=driver, url=url, class_name='jobCard_mainContent', timeout=10, refetch_times=3)
+        driver=driver, url=url, class_name='jobCard_mainContent', timeout=15, refetch_times=2)
