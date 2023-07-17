b6fefbb00df60e88ddaabd67cc8187541e4d6583
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-17 15:07:59 -0400

    scraper_fetch.py Implemented fetch_indeed() to handle fetching logic for Indeed job board

--- scraper/scraper_fetch.py
+++ scraper/scraper_fetch.py
@@ -1,35 +1,17 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import random
-from handlers.exceptions_handlers import timeout_exceptions_handler
+from util.util import fetch_wait_class
+from handlers.exceptions_handlers import NoResultsException
 
-# Make webdriver wait until class loads, otherwise raise Timeout Exception
-@timeout_exceptions_handler
-def webdriver_wait_class(driver: WebDriver, timeout: int, class_name: str):
-    # Increase timeout by a random number
-    timeout += random.random()
-    WebDriverWait(driver, timeout=timeout).until(
-        EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-    
-
-# Fetches url, wait until class loads, and refetches specified number of times after specified timeout
-def webdriver_fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, refetch_times: int = 0):
-    # Fetch url
-    driver.get(url)
-    
-    exception = TimeoutException()
-    # Wait for page to load, and if it does, shortcircuit function and return
-    # Otherwise, after specified refetch_times, grab the most recent exception and raise it
-    for i in range(refetch_times):
+# Fetches indeed, and raises a no results exception if there are no results, or a timeout exxception if the jobs do not load correctly
+def fetch_indeed(driver: WebDriver, url: str, initial_fetch: bool = False):
+    if initial_fetch:
         try:
-            webdriver_wait_class(driver = driver, timeout=timeout, class_name = class_name)
-            return
-        except TimeoutException as e:
-            exception = e
-            continue
+            fetch_wait_class(
+            driver=driver, url=url, class_name='jobsearch-NoResult-messageContainer', timeout=10)
+            raise NoResultsException()
+        except TimeoutException:
+            pass
     
-    # Raise most recent timeout exception
-    raise exception
+    fetch_wait_class(
+        driver=driver, url=url, class_name='jobCard_mainContent', timeout=10, refetch_times=3)
