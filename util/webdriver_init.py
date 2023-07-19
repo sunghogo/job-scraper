64893ab12fe63e2ceda6afef2c8a26cafcf2b011
sunghogo <leinad5592@gmail.com>
2023-07-19 12:25:27 -0400

    webdriver_init.py: Formatting

--- util/webdriver_init.py
+++ util/webdriver_init.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
 
+
 # Initializes Selenium chromium browser and settings
 def init_webdriver():
     # Setup selenium chrome options
@@ -14,11 +15,13 @@ def init_webdriver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     # Setting logging level for browser to 'WARNING' and above
     chrome_options.add_argument("--log-level=3")
-    chrome_options.set_capability('goog:loggingPrefs', { 'browser':'WARNING' })
+    chrome_options.set_capability('goog:loggingPrefs', {'browser': 'WARNING'})
 
     # Initialize webdriver
-    driver = webdriver.Chrome(options = chrome_options)
-    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+                           "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.execute_script(
+        "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
     return driver
