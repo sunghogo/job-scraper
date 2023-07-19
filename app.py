d6e24e5766439557ed720a5ddcd8ad2a5019ffe0
sunghogo <leinad5592@gmail.com>
2023-07-19 10:26:01 -0400

    app.py: Added Software Engineer in United States search

--- app.py
+++ app.py
@@ -7,15 +7,20 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
-    scrape(search_position = "Frontend Internship", search_location = "United States")
-    scrape(search_position = "Software Engineer Internship", search_location = "United States")
+    scrape(search_position="Frontend Internship",
+           search_location="United States")
+    scrape(search_position="Software Engineer Internship",
+           search_location="United States")
     scrape(search_position="Software Engineer",
-                  search_location="New York State", experience_level = "ENTRY_LEVEL")
-    scrape(search_position = "Frontend Developer", search_location = "United States", experience_level = "ENTRY_LEVEL")
-    scrape(search_position = "Full Stack Developer", search_location = "United States", experience_level = "ENTRY_LEVEL")
-    scrape(search_position = "Jr Developer", search_location = "United States", experience_level = "ENTRY_LEVEL")
-    # scrape_search(search_position="Software Engineer",
-    #               search_location="United States", experience_level = "ENTRY_LEVEL")
+           search_location="New York State", experience_level="ENTRY_LEVEL")
+    scrape(search_position="Frontend Developer",
+           search_location="United States", experience_level="ENTRY_LEVEL")
+    scrape(search_position="Full Stack Developer",
+           search_location="United States", experience_level="ENTRY_LEVEL")
+    scrape(search_position="Jr Developer",
+           search_location="United States", experience_level="ENTRY_LEVEL")
+    scrape(search_position="Software Engineer",
+           search_location="United States", experience_level="ENTRY_LEVEL")
     return render_template('index.html')
 
 
