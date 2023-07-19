e8b2e8f56efa85ac3aca01cf0fbe8e713edc3eb3
sunghogo <leinad5592@gmail.com>
2023-07-19 11:17:39 -0400

    util.py: filename args are now all lower-cased

--- util/util.py
+++ util/util.py
@@ -15,7 +15,7 @@ logs_path = f"{outputs_path}/logs"
 # Writes json data in outputs data directory, returns filepath name
 def write_json_data(data: List[Dict[str, str]], filename: str, filepath: str = "") -> str:
     if filepath == "":
-        full_filename = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.json"
+        full_filename = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename.lower()}.json"
         filepath = f"{data_path}/{full_filename}"
     with open(filepath, 'w') as file:
         json.dump(data, file, indent=4)
@@ -25,7 +25,7 @@ def write_json_data(data: List[Dict[str, str]], filename: str, filepath: str = "
 # Opens json file in outputs data directory, returns the json data
 def read_json_data(filename: str, filepath: str = "") -> List[Dict[str, str]]:
     if filepath == "":
-        filepath = f"{data_path}/{filename}.json"
+        filepath = f"{data_path}/{filename.lower()}.json"
     with open(filepath, 'r') as file:
         return json.load(file)
 
@@ -33,14 +33,14 @@ def read_json_data(filename: str, filepath: str = "") -> List[Dict[str, str]]:
 # Delete json file in outputs data directory
 def delete_json_data(filename: str, filepath: str = ""):
     if filepath == "":
-        filepath = f"{data_path}/{filename}.json"
+        filepath = f"{data_path}/{filename.lower()}.json"
     if os.path.exists(filepath):
         os.remove(filepath)
 
 
 # Writes/Appends to logs in outputs logs directory
 def append_log(data: str, log_type: str, filename: str):
-    full_filename = f"{datetime.now().strftime('%Y-%m-%d')}_{filename}.log"
+    full_filename = f"{datetime.now().strftime('%Y-%m-%d')}_{filename.lower()}.log"
     filepath = ""
     if log_type == "error":
         filepath = f"{errors_path}/{full_filename}"
