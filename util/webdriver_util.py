e794574f32de992425673ba221c182919208290b
sunghogo <leinad5592@gmail.com>
2023-07-19 11:31:26 -0400

    webdriver_util.py: screenshot() filename is now lower-cased

--- util/webdriver_util.py
+++ util/webdriver_util.py
@@ -7,21 +7,22 @@ from util.util import screenshots_path
 from datetime import datetime
 import random
 
+
 # Make webdriver wait until class loads, otherwise raises timeout exception
 def wait_class(driver: WebDriver, timeout: int, class_name: str):
     timeout += random.random()
     WebDriverWait(driver, timeout=timeout).until(
         EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-    
 
-# Makes webdriver fetch url, wait until class loads, and refetches specified number of times 
+
+# Makes webdriver fetch url, wait until class loads, and refetches specified number of times
 # If timing out all the refetches, raises most recent timeout exception
-def fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, fetch_times: int = 1):
+def fetch_wait_class(driver: WebDriver, url: str, class_name: str, timeout: int, fetch_times: int = 1):
     driver.get(url)
     timeout_exception = TimeoutException()
     for i in range(fetch_times):
         try:
-            wait_class(driver = driver, timeout=timeout, class_name = class_name)
+            wait_class(driver=driver, timeout=timeout, class_name=class_name)
             return
         except TimeoutException as e:
             timeout_exception = e
@@ -32,5 +33,5 @@ def fetch_wait_class(driver: WebDriver, url:str, class_name: str, timeout: int, 
 # Make webdriver screenshot and save the image in outputs screenshots directory
 # webdriver.set_window_size() crashes the page
 def screenshot(driver: WebDriver, filename: str):
-    filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename}.png"
+    filepath = f"{screenshots_path}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename.lower()}.png"
     driver.save_screenshot(filepath)
