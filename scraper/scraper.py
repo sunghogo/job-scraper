76ed724d2fd32094e0e5e8568dfd107939bee512
sunghogo <leinad5592@gmail.com>
2023-07-19 12:45:02 -0400

    scraper.py: Implemented basic scraper queue containing scrape calls

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,21 @@
 from scraper.scrape_indeed import scrape_indeed
 from util.webdriver_init import init_webdriver
+from queue import Queue
+
+scraper_queue = Queue()
+
 
 def scrape(search_position: str, search_location: str, experience_level: str = "ALL"):
     # Initialize webdriver instance
     driver = init_webdriver()
 
+    # Put scrape call into queue
+    scraper_queue.put(scrape_indeed, {driver: driver, search_position: search_position, search_location: search_location, search_options: {
+                      "experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"}})
+
     # Start scraping
-    scrape_indeed(driver = driver, search_position = search_position, search_location = search_location, search_options={"experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
-    
+    # scrape_indeed(driver=driver, search_position=search_position, search_location=search_location, search_options={
+    #               "experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
+
     # Close webdriver
     driver.quit()
