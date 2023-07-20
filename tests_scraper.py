40934b1ee3e6dd4ccbcb9910537c2bbc0641497a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-20 17:44:54 -0400

    tests_scraper.py: Minor fixes regarding .stop() thread termination, created test case for Scraper queue execution loop

--- tests_scraper.py
+++ tests_scraper.py
@@ -4,10 +4,10 @@
 import unittest
 import pytest
 from scraper.scraper import Scraper
-import logging
+import time
 
 class TestScraperMethods(unittest.TestCase):
-    # # Initialize Scraper module
+    # Initialize Scraper module
     scraper = Scraper()
     
     # Test whether scraper module was properly initialiszed
@@ -19,15 +19,9 @@ class TestScraperMethods(unittest.TestCase):
     def test_stop(self):
         self.scraper.stop()
         self.assertTrue(self.scraper.stop_event.is_set())
-    
-    # Test whether scraper module resume works
-    def test_resume(self):
-        self.scraper.resume()
-        self.assertFalse(self.scraper.stop_event.is_set())
 
     # Test whether adding scrape calls to the queue works
     def test_add_scrape(self):
-        self.scraper.stop()
         self.scraper.add_scrape(search_position="Software Engineer",
            search_location="New York", experience_level="ENTRY_LEVEL")
         self.assertFalse(self.scraper.queue.empty())
@@ -37,6 +31,17 @@ class TestScraperMethods(unittest.TestCase):
         self.scraper.execute_scrape()
         self.scraper.queue.task_done()
         self.assertTrue(self.scraper.queue.empty())
+    
+    # Since debugger keeps track of inactive threads, cannot determine whether thread closes properly through .stop() or .join(), and threading.active_count()
+    
+    # Test whether adding a scrape call to the queue automatically executes it through the run loop
+    def test_scraper_loop(self):
+        self.scraper = Scraper()
+        self.scraper.add_scrape(search_position="Software Engineer",
+           search_location="Austin, TX", experience_level="ENTRY_LEVEL")
+        self.scraper.queue.join()
+        self.assertTrue(self.scraper.queue.empty())
+            
 
 if __name__ == '__main__':
     unittest.main()
