4095403182fff4f076ddaf994b7554d91d5e3f1a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-20 10:28:00 -0400

    scraper.py: Refactored scraper.py module into a working thread class that continuously calls scrapes from internal queue

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,45 @@
+import threading
 from queue import Queue
+import time
 from scraper.scrape_indeed import scrape_indeed
 from util.webdriver_init import init_webdriver
+from handlers.exceptions_handlers import exceptions_handler
+from handlers.logs_handlers import logs_scraper_queue_handler
 
-scraper_queue = Queue()
+
+class scraper(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.queue = Queue()
+        # This ensures the thread will exit when the main program exits
+        self.daemon = True
+        # Create flag that stops the thread when set
+        self.stop_event = threading.Event()
+        self.start()
+    
+    # Stop thread
+    def stop(self):
+        self.stop_event.set()
+
+    # Starts thread
+    def run(self):
+        while not self.stop_event.is_set():
+            if not self.queue.empty():
+                try:
+                    self.queue.get()()
+                except Exception as e:
+                    print(f"Error executing function {func.__name__}: {e}")
+                finally:
+                    self.queue.task_done()
+
+    def add_scrape(self, search_position: str, search_location: str, experience_level: str = "ALL"):
+        self.queue.put(scrape, {search_position: search_position, search_location: search_location, experience_level: experience_level})
+    
+    @logs_scraper_queue_handler
+    @exceptions_handler
+    def call_scrape(self):
+        self.queue.get()()
+
 
 # Scrapes search on job boards
 def scrape(search_position: str, search_location: str, experience_level: str = "ALL"):
@@ -15,13 +52,3 @@ def scrape(search_position: str, search_location: str, experience_level: str = "
 
     # Close webdriver
     driver.quit()
-
-
-# Put scrape call into queue
-def scraper_put(search_position: str, search_location: str, experience_level: str = "ALL"):
-    scraper_queue.put(scrape, {search_position: search_position, search_location: search_location, experience_level: experience_level})
-    
-# Retrieves scrape calls from the queue and executes it
-def scraper_get():
-    function = scraper_queue.get()
-    function()
