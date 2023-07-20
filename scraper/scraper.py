572173ee4cbb2931c8154b305b2603cd64606a1a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-20 09:36:25 -0400

    scraper.py: scraper_get to retreive and execute function calls from the queue

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,10 @@
+from queue import Queue
 from scraper.scrape_indeed import scrape_indeed
 from util.webdriver_init import init_webdriver
-from queue import Queue
 
 scraper_queue = Queue()
 
-
+# Scrapes search on job boards
 def scrape(search_position: str, search_location: str, experience_level: str = "ALL"):
     # Initialize webdriver instance
     driver = init_webdriver()
@@ -15,7 +15,13 @@ def scrape(search_position: str, search_location: str, experience_level: str = "
 
     # Close webdriver
     driver.quit()
-    
-def scraper_push(search_position: str, search_location: str, experience_level: str = "ALL"):
-    # Put scrape call into queue
+
+
+# Put scrape call into queue
+def scraper_put(search_position: str, search_location: str, experience_level: str = "ALL"):
     scraper_queue.put(scrape, {search_position: search_position, search_location: search_location, experience_level: experience_level})
+    
+# Retrieves scrape calls from the queue and executes it
+def scraper_get():
+    function = scraper_queue.get()
+    function()
