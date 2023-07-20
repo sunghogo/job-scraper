6df4e76bbfac56fa264e5319fc083a3aa03d5cc6
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-20 09:25:45 -0400

    scraper.py: Abstracted scraper_queue scrape call push into scraper_push()

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,13 +9,13 @@ def scrape(search_position: str, search_location: str, experience_level: str = "
     # Initialize webdriver instance
     driver = init_webdriver()
 
-    # Put scrape call into queue
-    scraper_queue.put(scrape_indeed, {driver: driver, search_position: search_position, search_location: search_location, search_options: {
-                      "experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"}})
-
     # Start scraping
-    # scrape_indeed(driver=driver, search_position=search_position, search_location=search_location, search_options={
-    #               "experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
+    scrape_indeed(driver=driver, search_position=search_position, search_location=search_location, search_options={
+                  "experience_level": experience_level, "date_posted": "1", "sort_date": "true", "filter_dupe": "0"})
 
     # Close webdriver
     driver.quit()
+    
+def scraper_push(search_position: str, search_location: str, experience_level: str = "ALL"):
+    # Put scrape call into queue
+    scraper_queue.put(scrape, {search_position: search_position, search_location: search_location, experience_level: experience_level})
