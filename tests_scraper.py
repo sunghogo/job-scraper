c530ef10fb6382575979726850b41d56cc8c5c0f
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-20 14:54:12 -0400

    Created unit tests for Scraper class

---
+++ tests_scraper.py
@@ -0,0 +1,37 @@
+# Command to run test:
+# pytest tests_scraper.py
+
+import unittest
+import pytest
+from scraper.scraper import Scraper
+
+class TestScraperMethods(unittest.TestCase):
+    # Initialize Scraper module
+    def setUp(self):
+        self.scraper = Scraper()
+    
+    # Test whether scraper module was properly initialiszed
+    def test_scraper_init(self):
+        self.assertIsInstance(self.scraper, Scraper)
+        self.assertTrue(self.scraper.queue.empty())
+        
+    # Test whether scraper module stop works
+    def test_stop(self):
+        self.scraper.stop()
+        self.assertTrue(self.scraper.stop_event.is_set())
+    
+    # Test whether scraper module resume works
+    def test_resume(self):
+        self.scraper.resume()
+        self.assertFalse(self.scraper.stop_event.is_set())
+
+    # Test whether adding scrape calls to the queue works
+    def test_add_scrape(self):
+        self.scraper.stop()
+        self.scraper.add_scrape(search_position="Software Engineer",
+           search_location="United States", experience_level="ENTRY_LEVEL")
+        self.assertFalse(self.scraper.queue.empty())
+
+
+if __name__ == '__main__':
+    unittest.main()
