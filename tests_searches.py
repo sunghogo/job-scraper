4673c6eeaf0a86bd57c4eddb502fc285ce5477ca
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-26 08:11:55 -0400

    test_searches.py: Removed redundant test searches

--- tests_searches.py
+++ tests_searches.py
@@ -12,18 +12,18 @@ class TestScraperMethods(unittest.TestCase):
     
     # Test multiple searches
     def test_scraper_loop(self):
-        self.scraper.add_scrape(search_position="Software Engineer Entry",
-           search_location="United States", experience_level="ENTRY_LEVEL")
-        self.scraper.add_scrape(search_position="Software Engineer Junior",
-           search_location="United States", experience_level="ENTRY_LEVEL")
-        self.scraper.add_scrape(search_position="Software Developer Entry",
-           search_location="United States", experience_level="ENTRY_LEVEL")
-        self.scraper.add_scrape(search_position="Software Developer Junior",
-           search_location="United States", experience_level="ENTRY_LEVEL")
-        self.scraper.add_scrape(search_position="Software Engineer Internship",
-           search_location="United States", experience_level="ENTRY_LEVEL")
-        self.scraper.queue.join()
-        self.assertTrue(self.scraper.queue.empty())
+      self.scraper.add_scrape(search_position="Software Engineer",
+   search_location="New York", experience_level="ENTRY_LEVEL")
+      self.scraper.add_scrape(search_position="Software Engineer",
+search_location="Austin, TX", experience_level="ENTRY_LEVEL")
+      self.scraper.add_scrape(search_position="Software Engineer Entry",
+         search_location="United States", experience_level="ENTRY_LEVEL")
+      self.scraper.add_scrape(search_position="Software Engineer Junior",
+         search_location="United States", experience_level="ENTRY_LEVEL")
+      self.scraper.add_scrape(search_position="Software Engineer Internship",
+         search_location="United States", experience_level="ENTRY_LEVEL")
+      self.scraper.queue.join()
+      self.assertTrue(self.scraper.queue.empty())
             
 
 if __name__ == '__main__':
