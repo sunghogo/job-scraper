eb5f17cebc61fd14bb794ea47e53348566fc58a1
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-31 15:43:00 -0400

    test_1_webdriver.py: Changed to catching general Exception for socket.io exception catching

--- tests/test_1_webdriver.py
+++ tests/test_1_webdriver.py
@@ -5,7 +5,6 @@ import unittest
 import pytest
 from util.webdriver_init import init_webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.common.exceptions import WebDriverException
 
 class TestWebdriver(unittest.TestCase):
     # Initialize webdriver
@@ -16,7 +15,7 @@ class TestWebdriver(unittest.TestCase):
         self.assertIsInstance(self.driver, WebDriver)
 
     def test_webdriver_close(self):
-        with self.assertRaises(WebDriverException):
+        with self.assertRaises(Exception):
             self.driver.quit()
             self.driver.get('http://127.0.0.1')
 
@@ -25,7 +24,7 @@ class TestWebdriver(unittest.TestCase):
         if self.driver:
             try:
                 self.driver.quit()
-            except WebDriverException: # Catch test_web_driver_close() exception
+            except Exception: # Catch test_web_driver_close() exception
                 pass
 
         
