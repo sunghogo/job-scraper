0a5bc38d658fb9fa8e988f62145eab50c0765f5a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-31 14:32:52 -0400

    test_2_scraper.py: Refactored tests to have proper setUp and tearDown initialization and shut down

--- tests/test_2_scraper.py
+++ tests/test_2_scraper.py
@@ -5,13 +5,14 @@ import unittest
 import pytest
 from scraper.scraper import Scraper
 
+# Since debugger keeps track of inactive threads, cannot determine whether thread closes properly through .stop() or .join(), and threading.active_count()
 class TestScraper(unittest.TestCase):
     # Initialize Scraper module
-    scraper = None
+    def setUp(self):
+        self.scraper = Scraper()
     
     # Test whether scraper module was properly initialiszed
     def test_scraper_init(self):
-        self.scraper = Scraper()
         self.assertIsInstance(self.scraper, Scraper)
         self.assertTrue(self.scraper.queue.empty())
         
@@ -20,27 +21,26 @@ class TestScraper(unittest.TestCase):
         self.scraper.stop()
         self.assertTrue(self.scraper.stop_event.is_set())
 
-    # Test whether adding scrape calls to the queue works
-    def test_add_scrape(self):
+    # Test whether adding/executing scrape calls to/from the queue works
+    def test_add_execute_scrape(self):
         self.scraper.add_scrape(search_position="Software Engineer",
            search_location="New York", experience_level="ENTRY_LEVEL")
         self.assertFalse(self.scraper.queue.empty())
-
-    # Test whether getting and executing scrape calls from the queue works
-    def test_execute_scrape(self):
         self.scraper.execute_scrape()
         self.scraper.queue.task_done()
         self.assertTrue(self.scraper.queue.empty())
     
-    # Since debugger keeps track of inactive threads, cannot determine whether thread closes properly through .stop() or .join(), and threading.active_count()
-    
     # Test whether adding a scrape call to the queue automatically executes it through the run loop
     def test_scraper_loop(self):
-        self.scraper = Scraper()
         self.scraper.add_scrape(search_position="Software Engineer",
            search_location="Austin, TX", experience_level="ENTRY_LEVEL")
         self.scraper.queue.join()
         self.assertTrue(self.scraper.queue.empty())
+        
+    # Shutdown Scraper module
+    def tearDown(self):
+        if self.scraper:
+            self.scraper.stop()
             
 
 if __name__ == '__main__':
