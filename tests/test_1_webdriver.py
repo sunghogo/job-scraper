de73b9bb15d7b4e3301db9da9085ad721d74d4ae
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-07-31 16:05:53 -0400

    test_1_webdriver.py: Fixed exception testing/handling

--- tests/test_1_webdriver.py
+++ tests/test_1_webdriver.py
@@ -5,27 +5,28 @@ import unittest
 import pytest
 from util.webdriver_init import init_webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from urllib3.exceptions import MaxRetryError
 
 class TestWebdriver(unittest.TestCase):
     # Initialize webdriver
     def setUp(self):
         self.driver = init_webdriver()
 
+    # Test webdriver initialization
     def test_init_webdriver(self):
         self.assertIsInstance(self.driver, WebDriver)
 
+    # Test webdriver shutdown
     def test_webdriver_close(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(MaxRetryError):
             self.driver.quit()
             self.driver.get('http://127.0.0.1')
 
     # Shutdown webdriver
+    # Calling .quit() on an already shutdown driver does not raise an exception
     def tearDown(self):
         if self.driver:
-            try:
-                self.driver.quit()
-            except Exception: # Catch test_web_driver_close() exception
-                pass
+            self.driver.quit()
 
         
 if __name__ == '__main__':
