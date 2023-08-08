1495da7ec43288ad57e3358a39f027d3b774ffc3
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-08-08 16:53:22 -0400

    test_3_searches.py: Removed Software Engineer seraches in NY, TX

--- tests/test_3_searches.py
+++ tests/test_3_searches.py
@@ -12,10 +12,6 @@ class TestSearches(unittest.TestCase):
     
     # Test multiple searches in a queue
     def test_scraper_loop(self):
-      self.scraper.add_scrape(search_position="Software Engineer",
-         search_location="New York", experience_level="ENTRY_LEVEL")
-      self.scraper.add_scrape(search_position="Software Engineer",
-         search_location="Austin, TX", experience_level="ENTRY_LEVEL")
       self.scraper.add_scrape(search_position="Software Engineer Entry",
          search_location="United States", experience_level="ENTRY_LEVEL")
       self.scraper.add_scrape(search_position="Software Engineer Junior",
