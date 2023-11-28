223bf4831490abec93a9f7c02a8f279fe4b57811
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-11-28 11:38:54 -0500

    extract_indeed.py: Initialized html attribute/class/id names at top

--- scraper/extract_indeed.py
+++ scraper/extract_indeed.py
@@ -12,6 +12,23 @@ from util.util import write_json_data
 from util.webdriver_util import wait_class
 from scraper.construct_url import construct_indeed_url
 
+# Initialize html attribute/class/id names
+jobs_summary_container_id_attribute = 'data-testid'
+jobs_summary_container_page_next_attribute = 'pagination-page-next'
+job_summary_container_div_class = 'job_seen_beacon'
+job_summary_container_id_attribute = 'data-jk'
+job_summary_container_link_attribute = 'href'
+job_summary_container_title_class = 'jobTitle'
+job_summary_container_company_class = 'companyName'
+job_summary_container_location_class = 'companyLocation'
+job_summary_container_date_class = 'date'
+job_summary_container_salary_class = 'salary-snippet-container'
+job_summary_container_estimated_salary_class = 'estimated-salary-container'
+job_detailed_container_div_class = 'jobsearch-RightPane'
+job_detailed_container_easy_apply_class = 'jobsearch-IndeedApplyButton-buttonWrapper is-embedded'
+job_detailed_container_body_div_class = 'jobsearch-BodyContainer'
+job_detailed_container_details_div_class = 'jobDetailsSection'
+job_detailed_container_description_div_class = 'jobDescriptionText'
 
 # Navigates through indeed search pages and extracts job listing data
 def extract_indeed_pages(driver: WebDriver, search_position: str, search_location: str, search_options: Dict[str, str] = None, total_page_num: int = 1) -> List[Dict[str, str]]:
@@ -22,7 +39,7 @@ def extract_indeed_pages(driver: WebDriver, search_position: str, search_locatio
     # Loop over and fetch each page of job lisitings
     for page in range(1, total_page_num+1):
         # Update search options with page num and construct search page indeed url
-        search_options["page"] = str(page)
+        search_options['page'] = str(page)
         page_url = construct_indeed_url(
             search_position, search_location, search_options)
 
@@ -46,7 +63,7 @@ def extract_indeed_pages(driver: WebDriver, search_position: str, search_locatio
             time.sleep(10 + random.random())
 
         # If there are no next page url due to indeed cutting off search results, end search early
-        if parsed_page_html.find('a', {'data-testid': 'pagination-page-next'}) is None:
+        if parsed_page_html.find('a', {jobs_summary_container_id_attribute: jobs_summary_container_page_next_attribute}) is None:
             break
 
     # Return list of job data
@@ -63,14 +80,14 @@ def extract_indeed_page(driver: WebDriver) -> List[Dict[str, str]]:
     parsed_page_html = BeautifulSoup(extracted_page_html, 'html.parser')
 
     # Find both source and parsed job listing elements
-    jobs = parsed_page_html.find_all('div', class_='job_seen_beacon')
-    jobs_els = driver.find_elements(By.CLASS_NAME, "job_seen_beacon")
+    jobs = parsed_page_html.find_all('div', class_= job_summary_container_div_class)
+    jobs_els = driver.find_elements(By.CLASS_NAME, job_summary_container_div_class)
 
     # Extract data from each job listing on page
     for i, job in enumerate(jobs):
         # Get job id
-        job_id = job.find('a').get('data-jk', None)
-        job_link = job.find('a').get('href', None)
+        job_id = job.find('a').get(job_summary_container_id_attribute, None)
+        job_link = job.find('a').get(job_summary_container_link_attribute, None)
 
         # Click on each job listing to open job details body description
         jobs_els[i].click()
@@ -78,7 +95,7 @@ def extract_indeed_page(driver: WebDriver) -> List[Dict[str, str]]:
         # Wait for righthand job details body description to load, otherwise return to scraper.py module to exit the webdriver
         try:
             wait_class(
-                driver=driver, timeout=15, class_name='jobsearch-BodyContainer')
+                driver=driver, timeout=15, class_name=job_detailed_container_div_class)
         except TimeoutException:
             return
 
@@ -86,31 +103,31 @@ def extract_indeed_page(driver: WebDriver) -> List[Dict[str, str]]:
         time.sleep(1.5 + random.random() + random.random()*2)
 
         # Extract text content of interest from lefthand job summary cards
-        position = job.find('h2', class_='jobTitle').get_text()
-        company = job.find('span', class_='companyName').get_text()
-        location = job.find('div', class_='companyLocation').get_text()
-        date = job.find('span', class_='date').find('span').get_text()
+        position = job.find('h2', class_=job_summary_container_title_class).get_text()
+        company = job.find('span', class_= job_summary_container_company_class).get_text()
+        location = job.find('div', class_=job_summary_container_location_class).get_text()
+        date = job.find('span', class_=job_summary_container_date_class).find('span').get_text()
         date_posted = datetime.now().strftime('%Y-%m-%d')
         if 'ago' in date:
             days_ago = int(re.find(r'\d+', date))
             date_posted -= timedelta(days_ago)
 
         # Extract text content for metadata bubble
-        salary = job.find('div', class_='salary-snippet-container')
+        salary = job.find('div', class_=job_summary_container_salary_class)
         if salary is not None:
             salary = salary.get_text()
-        estimated_salary = job.find('div', class_='estimated-salary-container')
+        estimated_salary = job.find('div', class_=job_summary_container_estimated_salary_class)
         if estimated_salary is not None:
             estimated_salary = estimated_salary.get_text()
 
         # Re-extract and parse html after righthand job details body description loads
         reextracted_html = driver.page_source
         reparsed_html = BeautifulSoup(reextracted_html, 'html.parser')
-        job_right_panel = reparsed_html.find('div', class_='jobsearch-RightPane')
+        job_right_panel = reparsed_html.find('div', class_=job_detailed_container_div_class)
 
         # Extracts if the listing is easy apply
         easy_apply = 'no'
-        easy_apply_button = job_right_panel.find('div', class_='jobsearch-IndeedApplyButton-buttonWrapper is-embedded')
+        easy_apply_button = job_right_panel.find('div', class_= job_detailed_container_easy_apply_class)
         if easy_apply_button is not None:
             easy_apply = 'yes'
 
@@ -122,14 +139,14 @@ def extract_indeed_page(driver: WebDriver) -> List[Dict[str, str]]:
         #         separator='\n', strip=True)
         
         # Extract righthand job details section
-        job_details = job_right_panel.find('div', {"id": "jobDetailsSection"})
+        job_details = job_right_panel.find('div', {'id': job_detailed_container_details_div_class})
         if job_details is not None:
             job_details = job_details.get_text(
                 separator='\n', strip=True)
 
         # Extract righthand job description
         job_description = job_right_panel.find(
-            'div', {"id": "jobDescriptionText"})
+            'div', {'id': job_detailed_container_description_div_class})
         if job_description is not None:
             job_description = job_description.get_text(
                 separator='\n', strip=True)
